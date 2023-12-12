aa4ec2ede94d300bc24badb049c9317e961def09
pablo castillo villalobos <danielzabalanava@gmail.com>
2023-12-12 12:50:25 -0400

    modified:   .env.example 	modified:   src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,15 @@ import { ConfigModule } from '@nestjs/config';
       database: process.env.MYSQL_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
+      ssl: process.env.MYSQL_SSL === 'true',
+      extra: {
+        ssl:
+          process.env.MYSQL_SSL === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
     }),
     CatsModule,
     BreedsModule,
